dab5098bac543de08114519381ab8830f4c74bf4
Luna Lometta <thesealovedthemoon@gmail.com>
2023-04-23 10:15:39 +0300

    Create sample_code.c

---
+++ minecraft/personal/rpc/sample_code.c
@@ -0,0 +1,8 @@
+{
+  {
+    Console.WriteLine("Hey, what the hell are you doing down here?!");
+    Console.WriteLine("Get topside!");
+    Console.WriteLine("I heard troops are coming in to save us!");
+    //Oh, Barney, you poor fucking bastard...
+  }
+}
