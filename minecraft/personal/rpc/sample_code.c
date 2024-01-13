0e37ad66a606cebe13db5ad7d7d95c26d33e6e77
Luna Lometta <thesealovedthemoon@gmail.com>
2024-01-13 04:22:33 +0200

    Update sample_code.c
    Why was it nested like that..? Also made the comment look nicer.

--- minecraft/personal/rpc/sample_code.c
+++ minecraft/personal/rpc/sample_code.c
@@ -1,8 +1,4 @@
-{
-  {
-    Console.WriteLine("Hey, what the hell are you doing down here?!");
-    Console.WriteLine("Get topside!");
-    Console.WriteLine("I heard troops are coming in to save us!");
-    //Oh, Barney, you poor fucking bastard...
-  }
-}
+Console.WriteLine("Hey, what the hell are you doing down here?!");
+Console.WriteLine("Get topside!");
+Console.WriteLine("I heard troops are coming in to save us!");
+// Oh, Barney, you poor fucking bastard...
